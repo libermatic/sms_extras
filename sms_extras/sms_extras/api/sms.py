8a78f97df5a1cea3612b8c0ab606c124705e738b
Sun Howwrongbum <sun@libermatic.com>
2018-09-24 01:52:21 +0530

    Add api method to send sms and save as Communication

--- sms_extras/sms_extras/api/sms.py
+++ sms_extras/sms_extras/api/sms.py
@@ -4,6 +4,8 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.core.doctype.sms_settings.sms_settings import send_sms
+from toolz import merge
 
 
 def get_sms_text(template_name, doc):
@@ -13,3 +15,27 @@ def get_sms_text(template_name, doc):
     if not template:
         return None
     return frappe.render_template(template, {'doc': doc})
+
+
+def request_sms(number, content, communication=None):
+    try:
+        # fix for json.loads casting to int during number validation
+        send_sms('"{}"'.format(number), content)
+        if communication:
+            frappe.get_doc(
+                merge(
+                    {
+                        'doctype': 'Communication',
+                        'communication_type': 'Communication',
+                        'communication_medium': 'SMS',
+                        'subject': 'SMS: {}'.format(number)
+                    },
+                    communication,
+                    {
+                        'phone_no': number,
+                        'content': content,
+                    }
+                )
+            ).insert()
+    except Exception:
+        frappe.log_error(frappe.get_traceback())
