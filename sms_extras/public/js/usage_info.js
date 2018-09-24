9c359e4bb82a268fe732974d7e0d327de2fe60e0
Sun Howwrongbum <sun@libermatic.com>
2018-09-25 03:34:03 +0530

    Usage Info: handle dom insertion better
    - update styles

--- sms_extras/public/js/usage_info.js
+++ sms_extras/public/js/usage_info.js
@@ -1,16 +1,13 @@
 frappe.pages['usage-info'].on_page_show = async function(wrapper) {
+  const section = $(wrapper).find('.layout-main-section');
   $(wrapper)
-    .find('.sms_extras-usage')
-    .parent()
+    .find('.sms_extra-sms-section')
     .remove();
   const { message: sms_usage } = await frappe.call({
     method: 'sms_extras.api.sms.get_usage',
+    freeze: true,
   });
   if (sms_usage) {
-    const ht = frappe.render_template('usage_info_add', sms_usage);
-    $(wrapper)
-      .find('.usage-info-section')
-      .last()
-      .after(ht);
+    $(frappe.render_template('usage_info_add', sms_usage)).appendTo(section);
   }
 };
