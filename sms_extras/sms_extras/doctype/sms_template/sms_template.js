82bdad11758469cdd58fb374e274eb92552cb343
Sun Howwrongbum <sun@libermatic.com>
2019-08-26 00:29:32 +0530

    feat: add script to SMS Template js

--- sms_extras/sms_extras/doctype/sms_template/sms_template.js
+++ sms_extras/sms_extras/doctype/sms_template/sms_template.js
@@ -1,6 +1,4 @@
 // Copyright (c) 2018, Libermatic and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('SMS Template', {
-  refresh: function(frm) {},
-});
+frappe.ui.form.on('SMS Template', sms_extras.scripts.sms_template);
