47b8615904f380f53276f78bfdb4b07bb7028df5
Sun Howwrongbum <sun@libermatic.com>
2019-08-27 13:46:57 +0530

    feat(SMS Template): add validation

--- sms_extras/sms_extras/doctype/sms_template/sms_template.py
+++ sms_extras/sms_extras/doctype/sms_template/sms_template.py
@@ -4,8 +4,20 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
+from frappe.utils import cint
 from frappe.model.document import Document
 
 
 class SMSTemplate(Document):
-    pass
+    def validate(self):
+        if self.type != "Transactional" and cint(self.auto_trigger):
+            frappe.throw(_("Only Transactional template can be auto-triggered"))
+        if cint(self.auto_trigger) and not self.ref_doctype:
+            frappe.throw(_("Reference DocType required for auto-triggered templates"))
+        if cint(self.auto_trigger) and not self.num_field:
+            frappe.throw(_("Recipient No Field required for auto-triggered templates"))
+        if cint(self.save_com) and not cint(self.auto_trigger):
+            frappe.throw(
+                _("Communications can only be saved for auto-triggered templates")
+            )
